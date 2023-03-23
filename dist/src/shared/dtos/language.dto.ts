1d97810828cbe682090c732fd6143cad0e426920
nguyencaonhan271201 <1959024@itec.hcmus.edu.vn>
2023-03-24 01:08:16 +0700

    [c] Fix failed deploy

--- dist/src/shared/dtos/language.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsEnum } from 'class-validator';
-import { LanguageEnum } from '../enums/language.enum';
-
-export class LanguageRequestDto {
-  @ApiProperty({ enum: LanguageEnum })
-  @IsEnum(LanguageEnum)
-  language: LanguageEnum;
-}
